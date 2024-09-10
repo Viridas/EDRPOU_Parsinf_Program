ac7f231164f7fb22792a313f2ce1550d05ef7e00
Urii Lutsenko <dlyawarfeisa@gmail.com>
2024-09-10 09:54:02 +0300

    а

--- CompanyInfoApi/obj/Debug/net8.0/CompanyInfoApi.AssemblyInfo.cs
+++ CompanyInfoApi/obj/Debug/net8.0/CompanyInfoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyInfoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cdf49b958775347497d9739e0d5261ca5a6f244")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyInfoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyInfoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
